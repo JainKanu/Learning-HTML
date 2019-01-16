973b475d5a9bb109d068636e791f1a57f9828121
Karan Jain <karan.jain740@gmail.com>
2019-01-16 19:05:19 +0530

    defect resolved

--- my_program/assets/js/main.js
+++ my_program/assets/js/main.js
@@ -1,8 +1,11 @@
 function printTable(){
-    var num;
+    var num,result ="";
     num=Number(document.getElementById('txtNumber').value);
+    var pTag= document.getElementById('pPrint');
     for(var i=1; i<=10; i++){
-        var pTag= document.getElementById('pPrint');
-        pTag.innerHTML += (num*i) + "<br/>" 
+        // result +=  (num*i) + "<br/>" ;
+        result += num+ " * " + i +" = "+ num*i + "<br/>";
     }
+    pTag.innerHTML = result;
+    console.log(result);
 }

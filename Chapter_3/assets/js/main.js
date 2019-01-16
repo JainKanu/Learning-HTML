eb4aa8fffdb8c1846e08f4126fd85ca46348edab
Karan Jain <karan.jain740@gmail.com>
2019-01-16 17:33:28 +0530

    table

--- Chapter_3/assets/js/main.js
+++ Chapter_3/assets/js/main.js
@@ -1,25 +1,10 @@
 (function(){
-//console.log("HI");
-var tableNumber =3,tableLimit=15;
+var tableNumber =5,tableLimit=15;
 makeTable(tableNumber,tableLimit);    
 })();
 function makeTable(table = 2,tableLimit=10){
-   //console.log("2*1=2");
-   //console.log("2*2=4"); 
-   var num = 1;
-//    var num = 1;
-//    console.log("2*1=2");
-//    console.log( table+ " * " + num +" = "+ table    );
-//    num++;
-//    console.log( table+ " * " + num +" = "+ table*num    );
-
-
+    var num = 1;
     for(num=1;num<=tableLimit;num++){
-//         console.log("i = "+ i);
-        console.log( table+ " * " + num +" = "+ table*num    );
+    console.log( table+ " * " + num +" = "+ table*num    );
     }
 }
-// 2*1=2
-// 2*2=4
-// 2*10=20
-// for(i=0;i> ;i++)
--- my_program/assets/js/main.js
+++ my_program/assets/js/main.js
@@ -1,10 +1,8 @@
-(function(){
-    var tableNumber =3,tableLimit=15;
-    makeTable(tableNumber,tableLimit);    
-    })();
-    function makeTable(table = 2,tableLimit=10){
-       var num = 1;
-        for(num=1;num<=tableLimit;num++){
-            console.log( table+ " * " + num +" = "+ table*num    );
-        }
+function printTable(){
+    var num;
+    num=Number(document.getElementById('txtNumber').value);
+    for(var i=1; i<=10; i++){
+        var pTag= document.getElementById('pPrint');
+        pTag.innerHTML += (num*i) + "<br/>" 
     }
+}

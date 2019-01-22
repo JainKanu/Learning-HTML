25e9c5e253087f0c15c3778f12111d2500333b11
Karan Jain <karan.jain740@gmail.com>
2019-01-22 17:06:20 +0530

    Jquery Document

---
+++ my_program/assets/js/details.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+        var mybuttons= $("p:button");
+        mybuttons.click(ShowHideMenu);
+        console.log(PAGECONTENT);
+        });
+
+function ShowHideMenu(){
+        if($(this).text() == "Haryana") haryanaPage(PAGECONTENT.Haryana);
+        if($(this).text() == "Punjab") punjabPage(PAGECONTENT.Punjab);
+}
+
+function haryanaPage(HARYANACONTENT){
+    $("p").text(HARYANACONTENT.title);
+    $("p").after(HARYANACONTENT.description);
+}
+function punjabPage(PUNJABCONTENT){
+    $("p").text(PUNJABCONTENT.title);
+    $("p").after(PUNJABCONTENT.description);
+    }
